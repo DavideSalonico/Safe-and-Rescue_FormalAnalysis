8518e004b9048e882eee9a9c20da3b46e7790358
DavideSalonico <davide.salonico@mail.polimi.it>
2024-07-04 14:29:16 +0200

    fixed generator (syntax errors + drone trajectories)

--- grid_generator.py
+++ grid_generator.py
@@ -20,7 +20,7 @@ class Grid:
         self.__fill_random_cells_cluster__(n_fires, 2)
         self.__fill_random_cells_border__(n_exits, 1)
         self.__fill_random_cells__(n_civilians, 3)
-        self.__fill_random_cells__(n_frs, 4)
+        self.__fill_random_cells__(n_frs, 8)
         self.trajectories = self.__build_drone_trajectories__()
         self.traj_lengths = [len(traj) for traj in self.trajectories]
         self.max_traj = max(self.traj_lengths)
@@ -58,7 +58,7 @@ class Grid:
 
         # Determine the number of clusters to fill based on the fill_cluster_percentage
         clusters_to_fill = math.ceil(count / (cluster_rows * cluster_cols))
-        print("Clusters to fill: " + str(clusters_to_fill))
+        #print("Clusters to fill: " + str(clusters_to_fill))
 
         # Randomly select clusters to fill
         all_clusters = [(r, c) for r in range(n_clusters_row) for c in range(n_clusters_col)]
@@ -110,11 +110,15 @@ class Grid:
     def __build_drone_trajectories__(self):
         trajectories = []
         for i in range(self.n_drones):
-            print("Calculating trajectory for drone " + str(i))
+            #print("Calculating trajectory for drone " + str(i))
             drone_trajectory = []
             for c in range(self.cols):
                 drone_trajectory.append((i * self.rows // self.n_drones, c))
-                print(drone_trajectory)
+                #print(drone_trajectory)
+            
+            for c in range(self.cols - 2, 0, -1):
+                drone_trajectory.append((i * self.rows // self.n_drones, c))
+                
             trajectories.append(drone_trajectory)
         return trajectories
     
@@ -140,7 +144,7 @@ class Grid:
                 grid_str += "  {" + ", ".join(map(str, row)) + "},\n"
             else:
                 grid_str += "  {" + ", ".join(map(str, row)) + "}\n"
-        grid_str += "}"
+        grid_str += "};"
         return grid_str
     
     def droneDeclaration(self):
@@ -154,7 +158,7 @@ class Grid:
             adjusted_trajectory_str = []
             for position in adjusted_trajectory:
                 if position == ("OUT_OF_MAP",):
-                    adjusted_trajectory_str.append("OUT_OF_MAP")
+                    adjusted_trajectory_str.append("OUT_OF_MAP;")
                 else:
                     adjusted_trajectory_str.append("{" + ", ".join(map(str, position)) + "}")
             
@@ -163,7 +167,7 @@ class Grid:
                 drone_str += "  {" + ", ".join(adjusted_trajectory_str) + "},\n"
             else:
                 drone_str += "  {" + ", ".join(adjusted_trajectory_str) + "}\n"
-        drone_str += "}"
+        drone_str += "};"
         return drone_str
     
     def displayDrones(self):
@@ -187,9 +191,9 @@ class Grid:
         declarations += prefix + "MAX_TRAJ = " + str(self.max_traj) + ";" + "                   // Max trajectory length\n"
         declarations += "\n// Grid layout\n\n"
         declarations += "const pos_t OUT_OF_MAP = { -1, -1 };\n"
-        declarations += "int grid[GRID_LENGTH][GRID_WIDTH] = " + self.gridDeclaration() + "\n"
+        declarations += "int grid[GRID_LENGTH][GRID_HEIGHT] = " + self.gridDeclaration() + "\n"
         declarations += "\n// Global arrays\n\n"
-        declarations += "int drone_trajectories[N_DRONES][MAX_TRAJ] = " + self.droneDeclaration() + "\n"
+        declarations += "pos_t drone_trajectories[N_DRONES][MAX_TRAJ] = " + self.droneDeclaration() + "\n"
         declarations += "int drone_traj_lengths[N_DRONES] = {" + ", ".join(map(str, self.traj_lengths)) + "};\n"
         print(declarations)
         
@@ -198,45 +202,3 @@ grid = Grid(rows=30, cols=20, n_exits=10, n_fires=60, n_civilians=6, n_frs=5, t_
 #grid.display()
 #grid.displayDrones()
 grid.generateDeclarations()
-
-"""
-    def __fill_cluster_cells__(self, count, value, cluster_size=3):
-        if count > self.rows * self.cols:
-            raise ValueError("Count exceeds the number of cells in the grid")
-        
-        # Calculate the number of clusters along rows and columns
-        clusters_rows = (self.rows + cluster_size - 1) // cluster_size
-        clusters_cols = (self.cols + cluster_size - 1) // cluster_size
-        
-        # Create a list of all clusters
-        all_clusters = [(cluster_row, cluster_col) for cluster_row in range(clusters_rows) for cluster_col in range(clusters_cols)]
-        
-        # Determine the number of clusters to select based on the count and cluster size
-        clusters_to_select = min(len(all_clusters), max(1, count // (cluster_size**2)))
-        
-        # Randomly select clusters
-        selected_clusters = random.sample(all_clusters, clusters_to_select)
-        
-        for cluster_row, cluster_col in selected_clusters:
-            # Calculate the actual number of rows and cols in the cluster (for edge cases)
-            actual_cluster_rows = min(cluster_size, self.rows - cluster_row * cluster_size)
-            actual_cluster_cols = min(cluster_size, self.cols - cluster_col * cluster_size)
-            
-        # Calculate the number of cells to fill in each selected cluster
-        cells_to_fill_per_cluster = min(actual_cluster_rows * actual_cluster_cols, max(1, count // clusters_to_select))
-        
-        # Generate all positions within the cluster
-        cluster_positions = [(cluster_row * cluster_size + row_offset, cluster_col * cluster_size + col_offset) 
-                             for row_offset in range(actual_cluster_rows) 
-                             for col_offset in range(actual_cluster_cols)]
-        
-        # Randomly select positions within the cluster to fill
-        selected_positions = random.sample(cluster_positions, cells_to_fill_per_cluster)
-        
-        for row, col in selected_positions:
-            self.set_value(row, col, value)
-    """
-    
-    #TODO: Implement a function to fill cells in clusters
-    #      - Take a percentage of total cells
-    #      - Take a fraction of the dimemsions
